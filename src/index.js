ece146e688fecce64330e0eabf51a9f1a2335fcb
hasnatab <38013792+hasnatab@users.noreply.github.com>
2020-12-30 11:13:39 -0500

    Login and Register Page layout for the safetie website

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//Challenge: Without moving the userIsRegistered variable,
+//Without moving the userIsRegistered variable,
 //1. Show Login as the button text if userIsRegistered is true.
 //Show Register as the button text if userIsRegistered is false.
 //2. Only show the Confirm Password input if userIsRegistered is false.
